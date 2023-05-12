cf2bcfdeb769c7e0df585bfea8adad00e2b66483
Sarah Spriggs <sarah1591@gmail.com>
2023-05-12 12:57:50 -0500

    Updates a lot of stuff.

---
+++ src/app/layouts/photos-container/photos-container.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-photos-container',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './photos-container.component.html',
+  styleUrls: ['./photos-container.component.scss']
+})
+export class PhotosContainerComponent {}
--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -5,13 +5,14 @@ import { Observable } from 'rxjs';
 import { Device } from 'src/app/core/models/device.interface';
 import { DeviceDetectorService } from 'src/app/core/services/device-detector.service';
 import { RsvpContainerComponent } from '../../layouts/rsvp-container/rsvp-container.component';
+import { PhotosContainerComponent } from '../../layouts/photos-container/photos-container.component';
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.scss'],
-  imports: [CommonModule, RsvpContainerComponent]
+  imports: [CommonModule, RsvpContainerComponent, PhotosContainerComponent]
 })
 export class HomepageComponent {
   public deviceType$: Observable<Device> = this.deviceDetectorService.deviceType$;

e83df648bf8080638f699ff33c392529287e18eb
ashujainvi <ajainvi@buffalo.edu>
2023-05-15 01:44:07 -0500

    Update page based theme and add css variables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { Observable, take } from 'rxjs';
 
 import { Device } from './core/models/device.interface';
 import { DeviceDetectorService } from './core/services/device-detector.service';
+import { NavigationEnd, NavigationStart, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -11,9 +12,37 @@ import { DeviceDetectorService } from './core/services/device-detector.service';
 })
 export class AppComponent implements OnInit {
   public deviceType$: Observable<Device> = this.deviceDetectorService.deviceType$;
-  constructor(private deviceDetectorService: DeviceDetectorService) {}
+  public pageClass: string = 'homepage';
+  @ViewChild('mainContainer') mainContainer: ElementRef;
+
+  constructor(
+    private deviceDetectorService: DeviceDetectorService,
+    private router: Router,
+    private renderer: Renderer2
+  ) {}
 
   ngOnInit(): void {
     this.deviceDetectorService.init();
+    this.initRouteChangeEvent();
+  }
+
+  private initRouteChangeEvent() {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.pageClass = this.getPageClass(event);
+      }
+    });
+  }
+
+  private getPageClass(event: any): string {
+    if (event.url.includes('/event')) {
+      return 'events';
+    } else if (event.url.includes('/travel')) {
+      return 'travel';
+    } else if (event.url.includes('/venue')) {
+      return 'venue';
+    } else {
+      return 'homepage';
+    }
   }
 }
